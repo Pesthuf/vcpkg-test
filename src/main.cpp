103b3ced1e59518245f6b24c70406fb04660b894
Pesthuf <nschicker@icloud.com>
2022-04-08 20:54:58 +0200

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <string_view>
+#include <boost/asio.hpp>
+#include <fmt/core.h>
+#include <sqlite3.h>
+
+
+int a(void *a, int b, char **c, char **d);
+
+int main() {
+    fmt::print("Default format: {}\n", 5);
+    return 0;
+}
